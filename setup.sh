4714e800f7df3e4b10b15cfc3297bbebb6ffce85
Diana Meca <dianameca@protonmail.com>
2024-10-30 08:37:17 -0400

    Fix

--- setup.sh
+++ setup.sh
@@ -26,22 +26,22 @@ create_symlink() {
     if [ "$answer" != "${answer#[Yy]}" ]; then
         rm -rf "$target"
         ln -s "$DOTFILES_DIR/$item" "$target"
-        echo -e "===\nReplaced $target with symlink.\n"
+        echo -e "===\nReplaced $target with symlink\n"
     else
         echo -e "Skipped $item.\n"
     fi
     else
         ln -s "$DOTFILES_DIR/$item" "$target"
-        echo -e "Created symlink for $item.\n"
+        echo -e "Created symlink for $item\n"
     fi
 }
 
 # create local gitconfig if it doesn't exist already
 if [ ! -f "$GITCONFIG_LOCAL" ]; then
     touch "$GITCONFIG_LOCAL"
-    echo -e "===\n.gitconfig_local created.\n"
+    echo -e "===\n.gitconfig_local created\n"
 else
-  echo -e "===\nSKIP: .gitconfig_local already exists.\n"
+  echo -e "===\nSKIP: .gitconfig_local already exists\n"
 fi
 
 for item in "${items[@]}"; do
@@ -50,16 +50,16 @@ done
 
 # check if Oh My Zsh is installed, provide option to install it
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo "===\nOh My Zsh is not installed. Install? (y/n)\n"
+  echo -e "===\nOh My Zsh is not installed. Install? (y/n)\n"
   read -r install_zsh
   if [ "$install_zsh" != "${install_zsh#[Yy]}" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo -e "Oh My Zsh installed."
+    echo -e "Oh My Zsh installed"
   else
-    echo -e "===\nSKIP: Oh My Zsh installation.\n"
+    echo -e "===\nSKIP: Oh My Zsh installation\n"
   fi
 else
-  echo -e "===\nSKIP: Oh My Zsh already installed.\n"
+  echo -e "===\nSKIP: Oh My Zsh already installed\n"
 fi
 
-echo -e "===\nSETUP COMPLETED.\n"
+echo -e "===\nSETUP COMPLETED\n"
